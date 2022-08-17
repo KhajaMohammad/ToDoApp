32540b7f7041078e93d3a7705b5679d517f3c446
Khaja Mohammad (Cognizant Technology Solutions) <nihalk78678@gmail.com>
2022-08-17 23:38:13 +0530

    Removed unused imports

--- src/App.js
+++ src/App.js
@@ -3,15 +3,12 @@ import Header from "./Components/Header";
 import Tasks from "./Components/Tasks";
 import AddTask from "./Components/AddTask"
 function App() {
-  //Toffle Reminder
+  //Toggle Reminder
   const toggleReminder =(id)=>{
     setTasks(tasks.map((task)=>
-      
       task.id===id?{...task,reminder:  !task.reminder}:task
-    
     )
     )
-  
 }
 //show form
 const [showAddTask,setShowAddTask] = useState(false);
@@ -21,10 +18,8 @@ const [showAddTask,setShowAddTask] = useState(false);
    setTasks(tasks.filter((task)=>task.id !== id))
   }
   const [tasks,setTasks] = useState([
-   
-   
 ]);
-//AddTast 
+//AddTask
 const addTask = (newtask)=>{
   const id=Math.floor(Math.random()*10000+1)
   const newTask={id,...newtask}
